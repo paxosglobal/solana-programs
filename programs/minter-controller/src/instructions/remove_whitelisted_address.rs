59ddc226b358f75b076e4aec15f8e224073b106b
Zach Petersen <zachary.petersen@itbit.com>
2024-10-28 22:26:35 -0500

    Use Token-2022

--- programs/minter-controller/src/instructions/remove_whitelisted_address.rs
+++ programs/minter-controller/src/instructions/remove_whitelisted_address.rs
@@ -1,8 +1,6 @@
 use {
     anchor_lang::prelude::*,
-    anchor_spl::{
-        token::{ Mint },
-    },
+    anchor_spl::token_interface::{Mint},
 };
 use crate::*;
 
@@ -20,7 +18,7 @@ pub struct RemoveWhitelistedAddress<'info> {
 
     // Mint account address is a PDA
     #[account()]
-    pub mint_account: Account<'info, Mint>,
+    pub mint_account: InterfaceAccount<'info, Mint>,
 
     #[account(
         has_one = admin,
